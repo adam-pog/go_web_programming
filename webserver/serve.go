f591c7aa376fd4907074fd2e4a1e5100e6e90a40
Adam Pogwizd <adampog77@gmail.com>
2019-06-09 13:48:39 -0500

    added handler

--- webserver/serve.go
+++ webserver/serve.go
@@ -5,12 +5,18 @@ import (
   "fmt"
 )
 
+type MyHandler struct{}
+
 func main(){
   server := http.Server{
     Addr: "localhost:8080",
-    Handler: nil,
+    Handler: &MyHandler{},
   }
 
   fmt.Println("Listening on 8080 over ssl...")
   server.ListenAndServeTLS("cert.pem", "key.pem")
+}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "hello World!")
 }
