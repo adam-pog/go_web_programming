d398aa65bd44ddbdcdf0d56dc16aa28e4c9c5521
Adam Pogwizd <adampog77@gmail.com>
2019-07-04 12:05:17 -0500

    chain handlers

--- webserver/serve.go
+++ webserver/serve.go
@@ -3,6 +3,8 @@ package main
 import (
   "net/http"
   "fmt"
+  "reflect"
+  "runtime"
 )
 
 func main(){
@@ -10,10 +12,18 @@ func main(){
     Addr: "localhost:8080",
   }
 
-  http.HandleFunc("/hello", hello)
-  http.HandleFunc("/world", world)
+  http.HandleFunc("/hello", log(hello))
+  http.HandleFunc("/world", log(world))
   fmt.Println("Listening on 8080 over ssl...")
   server.ListenAndServeTLS("cert.pem", "key.pem")
+}
+
+func log(handler http.HandlerFunc) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    name := runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
+    fmt.Println("Handler function called - " + name)
+    handler(w, r)
+  }
 }
 
 func hello(w http.ResponseWriter, r *http.Request){
